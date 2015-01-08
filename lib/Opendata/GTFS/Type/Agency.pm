e3ee45c186e17bb41b23c58599ce9cdf4a5d98fc
Erik Carlsson <info@code301.com>
2015-01-08 18:43:30 +0100

    Namechange

---
+++ lib/Opendata/GTFS/Type/Agency.pm
@@ -0,0 +1,33 @@
+use Opendata::GTFS::Standard::Moops;
+
+# VERSION
+# PODNAME: Opendata::GTFS::Type::Agency
+# ABSTRACT:
+
+use Archive::Extract;
+use File::Temp;
+
+class Opendata::GTFS::Type::Agency using Moose {
+
+    my @columns = qw/
+        0 agency_id
+        1 agency_name
+        1 agency_url
+        1 agency_timezone
+        0 agency_lang
+        0 agency_phone
+        0 agency_fare_url
+    /;
+
+    for (my $i = 0; $i < $#columns; $i += 2) {
+        my $required = $columns[$i];
+        my $column = $columns[$i + 1];
+
+        has $column => (
+            is => 'ro',
+            isa => ($required ? Str : Maybe[Str]),
+        );
+    }
+    
+
+}
