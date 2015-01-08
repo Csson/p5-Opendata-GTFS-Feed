34fb278be96d23ba1d732925481bd6fb7623a8d5
Erik Carlsson <info@code301.com>
2015-01-08 11:25:21 +0100

    Init

---
+++ lib/Opendata/GTFS/Feed.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Opendata::GTFS::Feed;
+
+# VERSION
+
+
+
+1;
+
+# ABSTRACT: Short intro
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Opendata::GTFS::Feed;
+
+=head1 DESCRIPTION
+
+Opendata::GTFS::Feed is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,11 @@
+use strict;
+use warnings FATAL => 'all';
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+
+use Opendata::GTFS::Feed;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
