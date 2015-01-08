34008b223bc469ddae81b865e817965fb71c5f7d
Erik Carlsson <info@code301.com>
2015-01-09 00:10:06 +0100

    Typo

---
+++ lib/Opendata/GTFS/Type/FareAttribute.pm
@@ -0,0 +1,29 @@
+use Opendata::GTFS::Standard::Moops;
+
+# VERSION
+# PODNAME: Opendata::GTFS::Type::FairAttribute
+# ABSTRACT:
+
+class Opendata::GTFS::Type::FairAttribute using Moose {
+
+    my @columns = qw/
+        1 fare_id
+        1 price
+        1 currency_type
+        1 payment_method
+        1 transfers
+        0 transfer_duration
+    /;
+
+    for (my $i = 0; $i < $#columns; $i += 2) {
+        my $required = $columns[$i];
+        my $column = $columns[$i + 1];
+
+        has $column => (
+            is => 'ro',
+            isa => ($required ? Str : Maybe[Str]),
+        );
+    }
+    
+
+}
